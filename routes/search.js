3b25a31b32561a79b67279d8fd733b818c838add
dev-002 <devansh.gupta985@gmail.com>
2023-02-11 19:27:13 +0530

    API query data card

--- routes/search.js
+++ routes/search.js
@@ -10,18 +10,16 @@ router.get('/', (req, res)=>{
 
 router.post('/', async (req, res)=>{
     
-    const url = `https://www.cheapshark.com/api/1.0/games?title=${req.body.name}&limit=60&exact=0`; // &steamAppID=35140
+    const searchName = req.body.name;
+    const url = `https://www.cheapshark.com/api/1.0/games?title=${searchName}&limit=60&exact=0`; // &steamAppID=35140
     await fetch(url)
         .then( response => response.json() )
             .then(data => { 
                 if(data.length === 0 || data === undefined)
                     res.send('No data found')
-                res.send(data) 
+                res.render('show', {data, searchName}) 
             })
-        .catch( err => {
-            console.log(err);
-            res.send(err)
-        })
+        .catch( err => res.send(err) )
 })
 
 module.exports = router
